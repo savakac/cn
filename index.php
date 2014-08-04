463fa44bdb67e84b4300f2f8cfbcfad5586725de
Jan Salva <jan.salva@me.com>
2014-08-04 23:00:25 +0200

    Set extra-small devices

--- index.php
+++ index.php
@@ -15,41 +15,64 @@
 	<!-- Wrapper container head panel and slide -->
 	<div class="container">
 
-		<div class="row menu-bar-one">
+		<div class="row menu-bar-one hidden-xs">
 
-			<div class="col-xs-4 menu-bar">
+			<div class="col-sm-4 col-md-3 menu-bar">
 				<div class="logo-bar">
 					<img src="images/logo_cn.jpg" alt="">
 				</div>
 			</div>
 
-			<div class="col-xs-2 info-panel1">
-				<a href="#">NEWSLETTER</a>
-			</div>
-			<div class="col-xs-2 info-panel2">
-				<a href="#">PRESS</a>
-			</div>
-			<div class="col-xs-2 info-panel3">
-				<a href="#">PARTNERI</a>
-			</div>
-		</div>
+			<div class="col-sm-5 col-md-3 col-sm-offset-1 col-md-offset-4">
 
-		<div class="row">
-			<div class="col-xs-6 col-xs-offset-4">
-				<form class="col-xs-6 form-search search-box" role="search">
+				<span class="info-panel">
+					<a href="#">NEWSLETTER</a>
+					<a href="#">PRESS</a>
+					<a href="#">PARTNERI</a>
+				</span>
+
+				<form class="form-search search-box" role="search">
 					<div class="form-group has-control has-feedback">
 						<input type="text" class="form-control input-search form-input" placeholder="Search">
 						<span class="glyphicon glyphicon-search form-control-feedback search-icon"></span>
 					</div>
 				</form>
+
 			</div>
 
-			<div class="col-xs-2 social-image">
-				<img src="images/facebook-icon.png" alt="">
-				<img src="images/gplus.png" alt="">
-				<img src="images/twitter.png" alt="">
+			<div class="col-md-2 social-network">
+				<a href="#" title="Facebook"><img src="images/facebook-icon.png" alt=""></a>
+				<a href="#" title="Google+"><img src="images/gplus.png" alt=""></a>
+				<a href="#" title="Twitter"><img src="images/twitter.png" alt=""></a>
+			</div>
+
+		</div>
+
+		<!-- Show component xs size -->
+		<div class="row menu-bar-one visible-xs">
+			<div class="col-xs-2 col-xs-offset-3 logo-bar-xs">
+				<img src="images/logo_cn.jpg" alt="">
 			</div>
 		</div>
+
+		<div class="row menu-bar-one-xs visible-xs">
+
+			<button class="btn btn-default menu-button-xs" type="button">
+				<span class="glyphicon glyphicon-align-justify"></span>
+			</button>
+
+			<button class="btn btn-default menu-button-xs" type="button">
+				<span class="glyphicon glyphicon-search"></span>
+			</button>
+
+		</div>
+
+		<div class="row social-network-xs visible-xs">
+			<a href="#" title="Facebook"><img src="images/facebook-icon.png" alt=""></a>
+			<a href="#" title="Google+"><img src="images/gplus.png" alt=""></a>
+			<a href="#" title="Twitter"><img src="images/twitter.png" alt=""></a>
+		</div>
+		<!-- End show xs size -->
 
 		<div class="row hidden-xs">
 			<div class="col-lg-12 slide">	
@@ -94,49 +117,43 @@
 			</div>
 		</div>
  
-		<div class="row event-menu">			
-			<div class="container menu">
-				<button class="navbar-toggle" data-toggle="collapse" data-target=".navCollapse">
-					<span class="glyphicon glyphicon-align-justify"></span>
-				</button>
-				<div class="collapse navbar-collapse navCollapse">
+		<div class="row event-menu hidden-xs">			
 
-					<div class="col-lg-2 col-md-2 col-sm-3 col-md-offset-1 event-menu-item">
-						<a href="#" type="button" class="btn btn-default btn-lg col-md-2 button-menu">
-							<span class="glyphicon glyphicon-calendar"></span>
-					<p>KALENDÁR AKCIÍ</p>
-						</a>
-					</div>			
-				
-					<div class="col-lg-2 col-md-2 col-sm-2 event-menu-item">
-						<a href="#" type="button" class="btn btn-default btn-lg col-md-2 button-menu">
-							<span class="glyphicon glyphicon-bullhorn"></span>
-							<p>AKTUALITY</p>
-						</a>
-					</div>
-		
-					<div class="col-lg-2 col-md-2 col-sm-2 event-menu-item">
-						<a href="#" type="button" class="btn btn-default btn-lg col-md-2 button-menu">
-							<span class="glyphicon glyphicon-picture"></span>
-							<p>FESTIVAL CESTOU NECESTOU</p>
-						</a>
-					</div>
-		
-					<div class="col-lg-2 col-md-2 col-sm-2 event-menu-item">
-						<a href="#" type="button" class="btn btn-default btn-lg col-md-2 button-menu">
-							<span class="glyphicon glyphicon-facetime-video"></span>
-							<p>PREDNÁŠKY</p>
-						</a>
-					</div>
-		
-					<div class="col-lg-2 col-md-2 col-sm-3 event-menu-item">
-						<a href="#" type="button" class="btn btn-default btn-lg col-md-2 button-menu">
-							<span class="glyphicon glyphicon-user"></span>
-							<p>PREDNÁŠAJÚCI</p>
-						</a>
-					</div>
-				</div>				
+			<div class="col-lg-2 col-md-2 col-sm-3 col-md-offset-1 event-menu-item">
+				<a href="#" type="button" class="btn btn-default btn-lg col-md-2 button-menu">
+					<span class="glyphicon glyphicon-calendar"></span>
+			<p>KALENDÁR AKCIÍ</p>
+				</a>
 			</div>			
+				
+			<div class="col-lg-2 col-md-2 col-sm-2 event-menu-item">
+				<a href="#" type="button" class="btn btn-default btn-lg col-md-2 button-menu">
+					<span class="glyphicon glyphicon-bullhorn"></span>
+					<p>AKTUALITY</p>
+				</a>
+			</div>
+
+			<div class="col-lg-2 col-md-2 col-sm-2 event-menu-item">
+				<a href="#" type="button" class="btn btn-default btn-lg col-md-2 button-menu">
+					<span class="glyphicon glyphicon-picture"></span>
+					<p>FESTIVAL CESTOU NECESTOU</p>
+				</a>
+			</div>
+		
+			<div class="col-lg-2 col-md-2 col-sm-2 event-menu-item">
+				<a href="#" type="button" class="btn btn-default btn-lg col-md-2 button-menu">
+					<span class="glyphicon glyphicon-facetime-video"></span>
+					<p>PREDNÁŠKY</p>
+				</a>
+			</div>
+		
+			<div class="col-lg-2 col-md-2 col-sm-3 event-menu-item">
+				<a href="#" type="button" class="btn btn-default btn-lg col-md-2 button-menu">
+					<span class="glyphicon glyphicon-user"></span>
+					<p>PREDNÁŠAJÚCI</p>
+				</a>
+			</div>
+
 		</div>
 
 	</div>
